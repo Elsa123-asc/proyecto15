27fd1c3840a5eae94484d4f9e6454245e6c87a4c
elsa <elsa@gmail.com>
2024-12-04 14:00:44 +0100

    Ultimo actualizado

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,7 @@
-console.log('Hola mundo... por segunda vez');
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+console.log('Hola mundo... desde el cielo una hermosa mañana');
+
+
+document.getElementById('boton').click = function() {
+    alert('Me voy a comer una pizza margarita');
+}
